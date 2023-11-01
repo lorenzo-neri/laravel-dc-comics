b45f1cb48ae30ca2d1184421d5c5ff826503aa3f
lorenzo-neri <lorenzo21neri@gmail.com>
2023-11-01 18:42:35 +0100

    add data in index.blade.php

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,10 +2,23 @@
 @section ('content')
 <main class=""></main>
     <div class="container">
-        <div class="row">
+        <div class="row row-cols-4">
             @foreach($comics as $key => $comic)
-                <div class="col">
-                    {{$comic['title']}}
+                <div class="col p-3">
+                    <div style="width: 280px; height: 650px; overflow:auto" class="card">
+                      <img style="width:;" src="{{$comic['thumb']}}" class="" alt="{{$comic['title']}}">
+                      <div class="card-body">       
+                        <div class="card-title">{{$comic['title']}}</div>
+                        <div class="card-subtitle mb-2 text-muted">{{$comic['series']}}</div>
+                        <p class="card-text">{{$comic['description']}}</p>
+                        <div class="card-subtitle mb-2 text-muted">{{$comic['price']}}</div>
+                        <div class="card-subtitle mb-2 text-muted">{{$comic['sale_date']}}</div>
+                        <div class="card-subtitle mb-2 text-muted">{{$comic['type']}}</div>
+                        <div class="card-subtitle mb-2 text-muted">Artists: {{$comic['artists']}}</div>
+                        <div class="card-subtitle mb-2 text-muted">Writers: {{$comic['writers']}}</div>
+                    </div>
+                    </div>
+                    
                 </div>
             @endforeach
         </div>
