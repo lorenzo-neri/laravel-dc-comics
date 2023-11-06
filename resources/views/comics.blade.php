1426d096586ff5170b67a3c492440acea2ff2917
lorenzo-neri <lorenzo21neri@gmail.com>
2023-11-06 14:44:55 +0100

    fix img in comics.blade.php

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,7 +6,12 @@
             @foreach($comics as $key => $comic)
                 <div class="col p-3">
                     <div style="width: 280px; height: 650px; overflow:auto" class="card">
-                      <img style="width:;" src="{{$comic['thumb']}}" class="" alt="{{$comic['title']}}">
+                        @if (str_contains($comic->thumb, 'http'))
+
+                                    <img width="" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                                    @else
+                                    <img width="" src="{{ asset('storage/' . $comic->thumb) }}">
+                                    @endif
                         <div class="card-body">       
                             <div class="card-title">{{$comic['title']}}</div>
                             <div class="card-subtitle mb-2 text-muted">{{$comic['series']}}</div>
